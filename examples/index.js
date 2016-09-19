6f05bfec3b3430b9ac57d61cd6495380f5fb8a21
yofreke <sparkyjoe94@aim.com>
2016-09-19 11:40:14 -0700

    pick c116b7e21ea9b07d088250bd35f8b9371e685d28

--- examples/index.js
+++ examples/index.js
@@ -24,6 +24,18 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
+// getUserMedia only works over https in Chrome 47+, so we redirect to https.
+// Also notify user if running from file.
+if (window.location.protocol === 'file:') {
+  alert('You seem to be running this example directly from a file. Note that these examples only work when served from a server or localhost due to canvas cross-domain restrictions.');
+} else if (
+  window.location.hostname !== 'localhost' &&
+  window.location.protocol !== 'https:'
+) {
+  window.location.protocol = 'https';
+}
+
+
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
